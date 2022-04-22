522400ca61cab326a6046fc9b37a15c10a5954ee
hkgnp <ben@pngs.cc>
2022-04-22 19:17:52 +0800

    deploying go

---
+++ go.mod
@@ -0,0 +1,29 @@
+module my-ecomap-backend-go
+
+// +heroku goVersion go1.15
+go 1.15
+
+require (
+	github.com/gofiber/fiber/v2 v2.32.0
+	github.com/joho/godotenv v1.4.0
+	go.mongodb.org/mongo-driver v1.4.6
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/aws/aws-sdk-go v1.34.28 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/klauspost/compress v1.15.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.35.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
+	github.com/xdg/stringprep v0.0.0-20180714160509-73f8eece6fdc // indirect
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
+	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/joho/godotenv"
+)
+
+type MongoInstance struct {
+	Client *mongo.Client
+	Db     *mongo.Database
+}
+
+var mg MongoInstance
+
+type Resource struct {
+	Id        string  `json:"id,omitempty" bson:"_id,omitempty"`
+	Category  string  `json:"category"`
+	Org       string  `json:"org"`
+	Postal    int     `json:"postal"`
+	Address   string  `json:"address"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+
+func Connect() error {
+	client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+
+	err = client.Connect(ctx)
+	db := client.Database(os.Getenv("DB_NAME"))
+
+	if err != nil {
+		return err
+	}
+	defer cancel()
+
+	mg = MongoInstance{
+		Client: client,
+		Db:     db,
+	}
+	return nil
+}
+
+func main() {
+	port := ":" + os.Getenv("PORT")
+	if port == "" {
+		port = ":5000"
+
+	}
+
+	if err := Connect(); err != nil {
+		log.Fatal(err)
+	}
+
+	godotenv.Load()
+
+	app := fiber.New()
+	app.Use(cors.New())
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		query := bson.D{{}}
+
+		cursor, err := mg.Db.Collection("resources").Find(c.Context(), query)
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+
+		var resources []Resource = make([]Resource, 0)
+
+		if err := cursor.All(c.Context(), &resources); err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+
+		return c.JSON(resources)
+	})
+	log.Fatal(app.Listen(port))
+}
